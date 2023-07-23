1ad7b7a61086d24daff2a32878cb33699e54e452
MCKIT77 <jakemckit@gmail.com>
2023-07-23 05:02:02 -0400

    fixed routes, added delete route

--- routes/notes.js
+++ routes/notes.js
@@ -66,4 +66,43 @@ router.post('/api/notes', (req, res) => {
 });
 
 
+router.delete('/api/notes/:id', (req, res) => {
+    const noteIdToDelete = req.params.id;
+
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the database:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            try {
+                let notes = JSON.parse(data);
+
+                // Find the index of the note with the specified ID
+                const indexToDelete = notes.findIndex((note) => note.id === noteIdToDelete);
+
+                if (indexToDelete === -1) {
+                    res.status(404).json({ error: 'Note not found' });
+                } else {
+                    // Remove the note from the array
+                    notes.splice(indexToDelete, 1);
+
+                    // Write the updated notes array back to the `db.json` file
+                    fs.writeFile(dbPath, JSON.stringify(notes), (writeErr) => {
+                        if (writeErr) {
+                            console.error('Error writing to the database:', writeErr);
+                            res.status(500).json({ error: 'Failed to write to the database' });
+                        } else {
+                            res.json({ message: 'Note deleted successfully' });
+                        }
+                    });
+                }
+            } catch (parseError) {
+                console.error('Error parsing JSON data:', parseError);
+                res.status(500).json({ error: 'Failed to parse JSON data' });
+            }
+        }
+    });
+});
+
+
 module.exports = router;
